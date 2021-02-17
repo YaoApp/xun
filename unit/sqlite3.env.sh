da8ab892fabc7ffa1395b9781763343c1244f4a9
Max <shadow.iqka@qq.com>
2021-02-17 16:30:50 +0800

    travis: set sqlite3 dsn=:memory:

--- unit/sqlite3.env.sh
+++ unit/sqlite3.env.sh
@@ -1,4 +1,5 @@
 export XUN_UNIT_LOG="/Users/max/Code/yao/xun/unit/sqlite.log"
 export XUN_UNIT_SQLITE3_DSN="file:///Users/max/Code/yao/xun/unit/sqlite-unittest.db"
+# export XUN_UNIT_SQLITE3_DSN=":memory:"
 export XUN_UNIT_DSN="sqlite3"
 export XUN_MODE="test"
